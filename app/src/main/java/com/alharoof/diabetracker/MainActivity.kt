3ed94293d7218f83a74de0683a8f3daf294f4126
Arslan Shoukat <iarslanshoukat@gmail.com>
2019-08-15 21:42:54 +0500

    Initial commit.

---
+++ app/src/main/java/com/alharoof/diabetracker/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.alharoof.diabetracker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.alharoof.diabetracker.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/alharoof/diabetracker/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.alharoof.diabetracker.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
